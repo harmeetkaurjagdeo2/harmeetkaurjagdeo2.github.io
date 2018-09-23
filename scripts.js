dc8c030bb591d3d7cab03b6d7ec34ec162310c58
harmeet <186450007@iitb.ac.in>
2018-09-23 16:15:32 +0530

    made changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 //alert("Hey!");
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
